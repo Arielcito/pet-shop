7101595a4cefa47abce1dce2670f7274843b3212
bk-sin <beckasin.mc@gmail.com>
2021-11-09 11:48:54 -0300

    fix: sliders de min y max

--- filtrando.js
+++ filtrando.js
@@ -1,17 +1,35 @@
 const cards = document.getElementById('cards')
-const tipo = document.title.indexOf('Farmacia') > -1 ? 'Medicamento' : 'Juguete'
-const dataFiltradaSorteada = data
-  .filter(x => x.tipo === tipo)
-  .sort((a, b) => a.stock - b.stock)
+const tipo = document.title.indexOf('Farmacia') > -1
+  ? 'Medicamento'
+  : 'Juguete'
 
-drawCards(tipo)
+    .filter(x => x.tipo === tipo)
+    .sort((a, b) => a.stock - b.stock)
 
-function drawCards (tipo) {
+const API_URL = 'https://apipetshop.herokuapp.com/api/articulos'
+const init = {
+  method: 'GET'
+}
+fetch(API_URL, init)
+  .then(res => res.json())
+  .then(data => {
+    const articulos = data.response
+    const dataFiltradaSorteada = articulos
+      .filter(x => x.tipo === tipo)
+      .sort((a, b) => a.stock - b.stock)
+
+    drawCards(dataFiltradaSorteada)
+    rangeFilter(dataFiltradaSorteada)
+
+    return (articulos, dataFiltradaSorteada)
+  })
+  .catch(err => err.message)
+
+function drawCards (array) {
   cards.innerHTML = ''
 
-  dataFiltradaSorteada.forEach(producto => {
-    if (producto.precio >= rangeFilter().minPrice && producto.precio <= rangeFilter().maxPrice) {
-      cards.innerHTML +=
+  array.forEach(producto => {
+    cards.innerHTML +=
     `<div class="col">
       <div class="card h-100">
         <img src="${producto.imagen}" class="card-img-top" alt="...">
@@ -25,26 +43,26 @@ function drawCards (tipo) {
         </div>
       </div>
     </div>`
-    }
-  })
+  }
+  )
 }
 
-rangeFilter()
-
-function rangeFilter () {
+function rangeFilter (array) {
   const maxPrice = document.getElementById('maxPrice')
   const minPrice = document.getElementById('minPrice')
   const slideMax = document.querySelector('.slideMax')
   const slideMin = document.querySelector('.slideMin')
-  const precios = data.sort((a, b) => a.precio - b.precio).map(a => a.precio)
+  const precios = array.map(a => a.precio).sort((a, b) => a.precio - b.precio)
 
   minPrice.oninput = () => {
     const value = minPrice.value
     slideMin.textContent = value
+    drawCards(array.filter(x => x.precio >= minPrice.value && x.precio <= maxPrice.value))
   }
   maxPrice.oninput = () => {
     const value = maxPrice.value
     slideMax.textContent = value
+    drawCards(array.filter(x => x.precio >= minPrice.value && x.precio <= maxPrice.value))
   }
 
   minPrice.setAttribute('max', precios.slice(precios.length - 1, precios.length))
@@ -54,13 +72,4 @@ function rangeFilter () {
   maxPrice.setAttribute('max', precios.slice(precios.length - 1, precios.length))
   maxPrice.setAttribute('min', precios[0])
   maxPrice.setAttribute('value', precios.slice(precios.length - 1, precios.length))
-
-  maxPrice.addEventListener('change', drawCards)
-  minPrice.addEventListener('change', drawCards)
-
-  const pricesObj = {
-    minPrice: minPrice.value,
-    maxPrice: maxPrice.value
-  }
-  return pricesObj
 }
