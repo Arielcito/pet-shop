54ab24dde203840405d59aa2e09be091eb9f94d4
Ariel Serato <Serato.arieli@gmail.com>
2021-11-10 18:46:23 -0300

    commi

--- scripts/carrito.js
+++ scripts/carrito.js
@@ -2,7 +2,16 @@ let productoCarrito = document.querySelector(".dropdown-menu")
 
 function agregarCarrito(){
     productoCarrito.innerHTML = `
-                                <h1>hpña</h1>
+                                <li class="d-inline"><button type="button" class="btn-close d-inline" aria-label="Close"></button></li>
+                                <li class="d-inline "><h2 class="d-inline fs-6"> Carrito de compras</h2></li>
+                                <li>
+                                    <img src="./assets/contactoperro.png" alt="" class="w-25">
+                                    <h3 class="fs-6 d-inline">Articulo perruno</h3>
+                                    <span class="material-icons text-end">
+                                    delete
+                                    </span>
+                                </li>
+                                <li class="d-inline">Subtotal(sin enío):</li>
     `
 }
 agregarCarrito()
