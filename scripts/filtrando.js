7806c23c53c34c10e48809b94c6b764e9932fa20
bk-sin <beckasin.mc@gmail.com>
2021-11-11 19:55:42 -0300

    guardado

--- scripts/filtrando.js
+++ scripts/filtrando.js
@@ -1,11 +1,11 @@
-// filtros
-let fav = document.querySelector('.fav')
-const info = document.getElementsByClassName('card-title')
+
 const cards = document.getElementById('cards')
 const inputBuscar = document.getElementById('buscador')
 const tipo = document.title.indexOf('Farmacia') > -1
   ? 'Medicamento'
-  : 'Juguete'
+  : document.title.indexOf('Juguetes') > -1
+    ? 'Juguete'
+    : 'Favoritos'
 
 const API_URL = 'https://apipetshop.herokuapp.com/api/articulos'
 const init = {
@@ -14,58 +14,63 @@ const init = {
 fetch(API_URL, init)
   .then(res => res.json())
   .then(data => {
+    drawCards(JSON.parse(localStorage.getItem('favs')))
     const articulos = data.response
-    const dataFiltradaSorteada = articulos
+    let dataFiltradaSorteada = articulos
+    /* if (tipo !== 'Favoritos') { */
+    dataFiltradaSorteada = articulos
       .filter(x => x.tipo === tipo)
       .sort((a, b) => a.stock - b.stock)
     drawCards(dataFiltradaSorteada)
     sortFilter(rangeFilter(dataFiltradaSorteada))
     rangeFilter(dataFiltradaSorteada)
     filtroCombinado(dataFiltradaSorteada)
-    // filtroBusqueda(dataFiltradaSorteada)
-
+    /* } */
+    /* const favBtn = document.getElementsByClassName('favo')
+    for (let i = 0; i < wall.length; i++) {
+      favBtn[i].addEventListener('click', function (e) {
+        getId(e, articulos)
+      })
+    } */
     return (articulos, dataFiltradaSorteada)
   })
-  .catch(err => err.message)
+  .catch(err => err)
 
 function drawCards (array) {
-/*   let local = localStorage.getItem("favoritos")
- */ cards.innerHTML = ''
+  cards.innerHTML = ''
   if (array.length > 0) {
     array.forEach(producto => {
       cards.innerHTML +=
-     `<div class=" col-lg-3 col-md-4 col-sm-6 id="${producto._id}">
-       <div class="card-back card h-100 carta shadow-lg mb-5 mt-3 rounded">
-         <img src="${producto.imagen}" class=" d-block mx-auto card-img-top imgSize w-75" alt="...">
-         <div class="card-body">
-           <a href="producto.html?id=${producto._id}" class="productoAnchor">
-           <h6 class="card-title">${producto.nombre}</h6>
-           </div>
-           </a>
-         <div class="card-footer d-flex justify-content-around">
-         <ul class="list-group">
-         <li class="list-group-item ">
-         <small class=" text-danger fs-3">${producto.stock > 5 ? 'Stock disponible!' : 'Ultimas unidades!'} </small>
-         </li>
-         <li class="list-group-item">
-         <small class="text-muted fs-4">$${producto.precio}</small>
-         </li>
-         </ul>
-         </div>
-         <div class="form-check d-flex justify-content-around">
-            <button type="button" class="btn btn-primary m-1 buy ">Comprar!</button>
+    `<div class= "col-lg-3 col-md-4 col-sm-6" id="${producto._id}">
+      <div class="card-back card h-100 carta shadow-lg mb-5 mt-3 rounded">
+        <img src="${producto.imagen}" class=" d-block mx-auto card-img-top imgSize w-75" alt="...">
+        <div class="card-body">
+          <a href="producto.html?id="${producto._id}" class="productoAnchor">
+          <h6 class="card-title">${producto.nombre}</h6>
           </div>
-         <div class="form-check d-flex justify-content-around position-absolute end-0 mt-1">
-            <input class="form-check-input fav hidden" type="checkbox" value="" id="${producto._id}" >
-            <label class="form-check-label" for="${producto._id}">
-            <span class="material-icons">
-              favorite
+          </a>
+        <div class="card-footer d-flex justify-content-around">
+        <ul class="list-group">
+        <li class="list-group-item ">
+        <small class=" text-danger fs-3">${producto.stock > 5 ? 'Stock disponible!' : 'Ultimas unidades!'} </small>
+        </li>
+        <li class="list-group-item">
+        <small class="text-muted fs-4">$${producto.precio}</small>
+        </li>
+        </ul>
+        </div>
+        <div class="form-check d-flex justify-content-around">
+          <button type="button" class="btn btn-primary m-1 buy ">Comprar!</button>
+          </div>
+        <div class="form-check d-flex justify-content-around position-absolute end-0 mt-1">
+            <span class="close favo" id="${producto._id}">&times;</span>
             </span>
             </label>
           </div>
-       </div>
-     </div>`
+      </div>
+    </div>`
     }
+
     )
   } else {
     cards.innerHTML = `
@@ -75,6 +80,36 @@ function drawCards (array) {
   `
   }
 }
+/*
+function getId (e, array) {
+  console.log(e.target.id)
+  const id = e.target.id
+  array.filter(x => x._id === e.target.id).forEach(producto => {
+    if (producto.nombre !== undefined) {
+      GuardarFavorito(producto._id, producto.nombre, producto.imagen, producto.precio, producto.stock, id)
+    }
+  })
+} */
+
+/* function GuardarFavorito (_id, nombre, imagen, precio, stock, id) {
+  const producto = {
+    _id: _id,
+    nombre: nombre,
+    imagen: imagen,
+    precio: precio,
+    stock: stock
+  }
+
+  const si = fav.findIndex(e => e._id === id)
+
+  fav.findIndex(e => e._id === id) > -1 ? fav.splice(si, 1) : fav.push(producto)
+
+  localStorage.setItem('favs', JSON.stringify(fav))
+}
+ */
+/* function pintarFavs () {
+  drawCards(JSON.parse(localStorage.getItem('favs')))
+} */
 
 function rangeFilter (array) {
   const maxPrice = document.getElementById('maxPrice')
@@ -130,89 +165,4 @@ function filtroCombinado (array) {
   inputBuscar.oninput = () => {
     drawCards(filtroBusqueda(sortFilter(rangeFilter(array))))
   }
-}
-
-// localstorage agregar articulos a la canasta y añadir a favoritos
-
-function addToCart (e) {
-  const producto = e.target.parentElement.parentElement.parentElement
-  const nombre = producto.querySelector('.card-title').textContent
-  const precio = producto.querySelector('.card-footer small:nth-child(3)').textContent
-  const imagen = producto.querySelector('.card-img-top').src
-  const stock = producto.querySelector('.card-footer small:nth-child(2)').textContent
-  const id = producto.querySelector('.card-footer small:nth-child(1)').textContent
-
-  const cart = JSON.parse(localStorage.getItem('cart')) || []
-  const newItem = {
-    nombre,
-    precio,
-    imagen,
-    stock,
-    id
-  }
-  cart.push(newItem)
-  localStorage.setItem('cart', JSON.stringify(cart))
-  alert('Producto agregado a la canasta')
-}
-
-function addToFav (e) {
-  const producto = e.target.parentElement.parentElement.parentElement
-  const nombre = producto.querySelector('.card-title').textContent
-  const precio = producto.querySelector('.card-footer small:nth-child(3)').textContent
-  const imagen = producto.querySelector('.card-img-top').src
-  const stock = producto.querySelector('.card-footer small:nth-child(2)').textContent
-  const id = producto.querySelector('.card-footer small:nth-child(1)').textContent
-
-  const fav = JSON.parse(localStorage.getItem('fav')) || []
-  const newItem = {
-    nombre,
-    precio,
-    imagen,
-    stock,
-    id
-  }
-  fav.push(newItem)
-  localStorage.setItem('fav', JSON.stringify(fav))
-  alert('Producto agregado a favoritos')
-}
-
-// LocalStorage
-
-function guardarLocalStorage (array) {
-  localStorage.setItem('fav', JSON.stringify(array))
-}
-
-function obtenerLocalStorage () {
-  const array = JSON.parse(localStorage.getItem('fav'))
-
-  fav = JSON.parse(localStorage.getItem('favs'))
-}
-
-function localStorage () {
-  const cart = JSON.parse(localStorage.getItem('cart')) || []
-  const fav = JSON.parse(localStorage.getItem('fav')) || []
-
-  cart.forEach(producto => {
-    const card = document.querySelector(`[data-id="${producto.id}"]`)
-    card.querySelector('.buy').textContent = 'Añadido'
-    card.querySelector('.buy').disabled = true
-  })
-
-  fav.forEach(producto => {
-    const card = document.querySelector(`[data-id="${producto.id}"]`)
-    card.querySelector('.fav').textContent = 'Añadido'
-    card.querySelector('.fav').disabled = true
-  })
-}
-
-function filtroBusqueda (productos) {
-  const texto = inputBuscar.value.toLowerCase()
-  const arrayBuscado = []
-  for (const producto of productos) {
-    const nombre = producto.nombre.toLowerCase()
-    if (nombre.indexOf(texto) !== -1) {
-      arrayBuscado.push(producto)
-    }
-  }
-  return arrayBuscado
 }
