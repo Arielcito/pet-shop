98f470eea1ae0bf077ad38633bff6bbdfff9756e
19037908 <moraga.hermosilla@hotmail.com>
2021-11-11 22:58:57 -0300

    feat:carrito

--- scripts/producto.js
+++ scripts/producto.js
@@ -1,6 +1,11 @@
 const container = document.querySelector(".container")
+
+
+
+// addProducto.addEventListener('click', renderCarrito)
+// console.log(API_URL);
 const API_URL = 'https://apipetshop.herokuapp.com/api/articulos'
-let cont = 0
+let cont = 1
 
 const init = {
   method: 'GET'
@@ -13,8 +18,10 @@ fetch(API_URL, init)
 
     let URLsearch = window.location.search
     let id = URLsearch.slice(4)
-
     cargarInterfaz(articulos, id)
+    manageCarrito(articulos, id)
+
+    // pModal(articulos, id)
     return articulos
   })
   .catch(err => err.message)
@@ -24,23 +31,6 @@ function cargarInterfaz(array, id) {
 
   container.innerHTML = `
     <div class="row m-4">
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
       <div class="col m-auto">
        <img src="${producto.imagen}" class="w-100 m-auto">
       </div>
@@ -49,9 +39,9 @@ function cargarInterfaz(array, id) {
         <h3 class="fw-light">&#36;${producto.precio}<h3>
         <p class="fs-5 fw-light" style="text-indent:20px;">${producto.descripcion}</p>
         <small>Cantidad disponible : ${producto.stock}<small>
-        <button type="button" class="btn btn-primary m-1 buy">Añadir a la canasta</button>
+        <button id="ad-carrito" class="btn btn-primary bottom-0 end-0 cart" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Anadir a la canasta </button>
         <div class="d-flex flex-column">
-          <h2>Cantidad: <span id="contador">0</span></h2>
+          <h2>Cantidad: <span id="contador">1</span></h2>
           <div>
             <button onclick="sumar('${producto.stock}')">+</button>
             <button onclick="restar()">-</button>
@@ -59,21 +49,89 @@ function cargarInterfaz(array, id) {
         </div>
       </div>
     </div>
-    <button id="btnmodal" hidden type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">sd</button>
-    
     `
 }
 function sumar(stock) {
-
   const contador = document.querySelector('#contador')
-
   cont < stock ? (cont++, contador.innerHTML = cont) : exceso()
 }
 function restar() {
-  cont > 0 && (cont--, contador.innerHTML = cont)
+  cont > 1 && (cont--, contador.innerHTML = cont)
 }
 
 function exceso() {
   const btnmodal = document.getElementById('btnmodal')
   btnmodal.click()
+}
+
+const array = []
+
+function manageCarrito(productos, id) {
+  let productoById = productos.find(producto => producto["_id"] == id)
+  const cantidad = document.querySelector('#contador')
+  const dModal = document.querySelector("#tabla-modal tbody")
+  const addProducto = document.querySelector('#ad-carrito')
+  const getLocal = getLocalStorage()
+
+
+  addProducto.addEventListener('click', renderCarrito)
+  // let result = getLocal.filter((item, index) => {
+  //   return getLocal.indexOf(item._id) === index;
+  // })
+  function renderCarrito() {
+    const obj = {
+      id: productoById._id,
+      total: Number(cantidad.innerText * productoById.precio),
+      precio: productoById.precio,
+      imagen: productoById.imagen,
+      stock: productoById.stock
+    }
+    array.push(obj, ...getLocal)
+
+    setLocalStorage(array)
+
+    array.forEach(element => {
+      dModal.innerHTML += `
+      <tr>
+          <td><img class="w-100" src="${element.imagen}" /></td>  
+          <td id="contadorCarrito">${cantidad.innerText}</td>  
+          <td>${element.precio}</td>  
+          <td id="set-total">${element.total}</td>  
+          <td>
+         </td>
+      </tr>
+      `
+    })
+
+    // console.log(productoById);
+  }
+}
+
+
+function sumarCarrito(stock, precio, id) {
+  console.log(id);
+  const setTotal = document.querySelector('#set-total')
+  const contadorCarrito = document.querySelector('#contadorCarrito')
+  // console.log(contadorCarrito);
+  cont < stock ? (cont++, contadorCarrito.innerHTML = cont, setTotal.innerHTML = cont * precio) : exceso()
+
+}
+
+function restarCarrito(precio) {
+  const setTotal = document.querySelector('#set-total')
+  cont > 1 && (cont--, contadorCarrito.innerHTML = cont, setTotal.innerHTML = cont * precio)
+}
+
+function eliminarElemento(id) {
+  // localStorage.getItem('car')
+}
+
+function getLocalStorage() {
+  const arrayCart = localStorage.getItem('cart') ? localStorage.getItem('cart') : '[]'
+  // console.log(arrayCar);
+  return JSON.parse(arrayCart)
+}
+
+function setLocalStorage(item) {
+  localStorage.setItem('cart', JSON.stringify(item))
 }
